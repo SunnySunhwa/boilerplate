3f7c7b6ecbee42e54df5bfd52af2a99591c435c6
SunnySunhwa <via1229@gmail.com>
2018-07-20 12:39:19 +1200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const API_KEY = 'AIzaSyDQK0tyOvaa9c0cNhQ1wjU9hrecdroKyy0';
+const App = () => {
+  return ( <div> Hi </div>);
+}
+
+ReactDom.render(<App />, document.querySelector('.container'));
